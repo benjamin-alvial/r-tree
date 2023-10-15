ac68fa4d0bdaf6171a365551dee6a477f72f369b
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2023-10-15 00:36:58 -0300

    feat: add function for printing ints from file

---
+++ printIntsFromFile.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+void printIntsFromFile(const char *filename, int x) {
+    FILE *file;
+
+    file = fopen(filename, "rb");  // Open the file in binary mode for reading
+
+    int num;
+    int count = 0;
+
+    while (fread(&num, sizeof(int), 1, file) == 1) {
+        printf("%d ", num);
+        count++;
+
+        if (count == x) {
+            printf("\n");
+            count = 0;
+        }
+    }
+
+    // Print a newline if the last line is not complete
+    if (count > 0) {
+        printf("\n");
+    }
+
+    fclose(file);
+}
