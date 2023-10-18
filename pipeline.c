ac5c0b021eb251de82bfc8fbe5bcff66b2e54515
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2023-10-18 01:56:11 -0300

    test: connect pipeline with method 1

--- pipeline.c
+++ pipeline.c
@@ -1,9 +1,10 @@
 #include "generateRectangleFile.c"
-#include “search.c”
+#include "search.c"
+#include "x-tree-2.c"
 
 // Powers of 2 for the size of original data rectangles
-#define N_POW_INITIAL 10 
-#define N_POW_FINAL 25
+#define N_POW_INITIAL 4 
+#define N_POW_FINAL 4
 
 // Domain in which the rectangles are defined
 #define DOMAIN_MIN 0
@@ -21,7 +22,7 @@
 // Update according to computer architecture:
 // If B is the size of a memory block,
 // then M = P/sizeof(int) - 4
-#define M 124
+#define M 4
 
 int main() {
 
@@ -31,25 +32,25 @@ int main() {
     // Array for the powers of n to test
     int n_values_size = N_POW_FINAL-N_POW_INITIAL+1;
     long n_values[n_values_size]; // Array for values of n (powers of 2);
-    for(i=0, i<n_values_size, i++) {
+    for(int i=0; i<n_values_size; i++) {
         n_values[i] = pow(2, i+N_POW_INITIAL);
     }
 
     // Iterate over the powers of n.
-    for(int i=0, i<n_values_size, i++) {
-        n = n_values[i];
+    for(int i=0; i<n_values_size; i++) {
+        int n = n_values[i];
         generateRectangleFile("rect_R.bin", n, DOMAIN_MIN, DOMAIN_MAX, R_SIDE_MIN, R_SIDE_MAX);
         generateRectangleFile("rect_Q.bin", Q_AMOUNT, DOMAIN_MIN, DOMAIN_MAX, Q_SIDE_MIN, Q_SIDE_MAX);
 
-        createTreeMethodOne("rect_R.bin", M); // Creates the tree_1.bin file
-        createTreeMethodTwo("rect_R.bin", M); // Creates the tree_2.bin file
-        createTreeMethodThree("rect_R.bin", M); // Creates the tree_3.bin file
+        createTreeMethodOne("rect_R.bin", n, M); // Creates the tree_1.bin file
+        //createTreeMethodTwo("rect_R.bin", M); // Creates the tree_2.bin file
+        //createTreeMethodThree("rect_R.bin", M); // Creates the tree_3.bin file
         
-        double time_1 = searchRectangleFile("rect_Q", "tree_1.bin", M);
-        double time_2 = searchRectangleFile("rect_Q", "tree_2.bin", M);
-        double time_3 = searchRectangleFile("rect_Q", "tree_3.bin", M);
+        //double time_1 = searchRectangleFile("rect_Q", "tree_1.bin", M);
+        //double time_2 = searchRectangleFile("rect_Q", "tree_2.bin", M);
+        //double time_3 = searchRectangleFile("rect_Q", "tree_3.bin", M);
 
-        fprintf(results_file, "%d, %lf, %lf, %lf\n", n, time_1, time_2, time_3); // To graph in another software
+        //fprintf(results_file, "%d, %lf, %lf, %lf\n", n, time_1, time_2, time_3); // To graph in another software
     }
 
     fclose(results_file);
